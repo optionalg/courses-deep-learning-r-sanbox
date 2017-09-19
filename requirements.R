65e0a602b8f5bd7e3eb8f4a14ea5b957be60c895
Rasmus Bååth <rasmus.baath@gmail.com>
2017-09-19 17:54:11 +0200

    Installing some more packages.

--- requirements.R
+++ requirements.R
@@ -5,8 +5,8 @@ library(devtools)
 # I'm not sure if this freezes the versions of the sub-libraries
 #install_version("tidyverse", "1.1.1") 
 #install_version("ggjoy", "0.3.0")
-install.packages("tidyverse")
-install.packages("ggjoy")
+install.packages("mlbench")
+install.packages("mlr")
 
 
 ##### Install packages in development from GitHub #####
